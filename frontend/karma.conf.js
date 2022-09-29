2f2861cd01f656247b17d5df787942a56c5ff674
NielsPrins <NielsPrins@users.noreply.github.com>
2022-09-29 15:33:48 +0200

    Pipeline af

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadlessNoSandbox'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
