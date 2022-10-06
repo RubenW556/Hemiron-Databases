7c196573a737d80c7e1a2f2b6cca16a2963bc168
NielsPrins <NielsPrins@users.noreply.github.com>
2022-10-06 15:07:27 +0200

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 cd databases
 
 docker pull registry.inf-hsleiden.nl/2223.ipsenh-p1-p2/databases/backend:latest
-docker pull registry.inf-hsleiden.nl/2223.ipsenh-p1-p2/databases/kdfn:latest
+docker pull registry.inf-hsleiden.nl/2223.ipsenh-p1-p2/databases/frontend:latest
 
 docker stack deploy -c docker-stack.yml databases
