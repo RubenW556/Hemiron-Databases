eeac12fba6c0a6d8a070cb82a38e3f3646a7ff2e
max.ottenhof@gmail.com <max.ottenhof@gmail.com>
2022-10-18 21:28:49 +0200

    deploy

---
+++ backend/deploy-backend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# navigate to folder
+cd /home/student/databases
+# create network if not exists
+docker network create -d overlay --attachable databases-backend
+# deploy stack
+docker stack deploy -c backend-stack.yml databases-backend --with-registry-auth --prune
