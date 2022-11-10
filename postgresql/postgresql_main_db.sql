01ef50430de5129ef61536eb4a2b0636481c8ba1
suley <s1115672@student.hsleiden.nl>
2022-11-11 00:24:29 +0100

    Code for creating the postgresql main database has been created

--- postgresql/postgresql_main_db.sql
+++ postgresql/postgresql_main_db.sql
@@ -1,14 +1,12 @@
 CREATE TABLE "Database" (
-	"database_id" uuid NOT NULL,
+	"database_id" serial NOT NULL,
 	"database_name" character varying(50) NOT NULL,
 	"database_type" character varying(15) NOT NULL,
-	"creation_date" DATE NOT NULL,
+	"creation_date" DATE(15) NOT NULL,
 	CONSTRAINT "Database_pk" PRIMARY KEY ("database_id")
 ) WITH (
   OIDS=FALSE
 );
-
-
 
 CREATE TABLE "User" (
 	"username" character varying(50) NOT NULL,
@@ -17,8 +15,6 @@ CREATE TABLE "User" (
 ) WITH (
   OIDS=FALSE
 );
-
-
 
 CREATE TABLE "Query" (
 	"resource_used" int NOT NULL,
@@ -30,21 +26,17 @@ CREATE TABLE "Query" (
   OIDS=FALSE
 );
 
-
-
 CREATE TABLE "User_Owns_Database" (
-	"user_id" uuid NOT NULL,
-	"database_id" uuid NOT NULL,
+	"user_id" bigint NOT NULL,
+	"database_id" bigint NOT NULL,
 	CONSTRAINT "User_Owns_Database_pk" PRIMARY KEY ("user_id","database_id")
 ) WITH (
   OIDS=FALSE
 );
 
 
-
-
-
 ALTER TABLE "Query" ADD CONSTRAINT "Query_fk0" FOREIGN KEY ("database_id") REFERENCES "Database"("database_id");
+
 ALTER TABLE "User_Owns_Database" ADD CONSTRAINT "User_Owns_Database_fk0" FOREIGN KEY ("user_id") REFERENCES "User"("user_id");
 ALTER TABLE "User_Owns_Database" ADD CONSTRAINT "User_Owns_Database_fk1" FOREIGN KEY ("database_id") REFERENCES "Database"("database_id");
 
