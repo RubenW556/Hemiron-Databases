ff463d61f58499a8da2608de595fe17303ea754c
Ruub Miner <s1127833@student.hsleiden.nl>
2022-11-17 12:03:20 +0100

    entrypoint.sql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,15 @@
-import { Module } from '@nestjs/common';
+import {Module, Post} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {configService} from "./config/config.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
+
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
-  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
 }
+
+
+
+
