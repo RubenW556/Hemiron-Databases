6010cf096e4b80fa541c05c298ee5ab77b423a1d
NielsPrins <NielsPrins@users.noreply.github.com>
2022-11-23 17:45:51 +0100

    HttpStatus codes

--- backend/src/databases/databases.controller.ts
+++ backend/src/databases/databases.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 import { createDatabaseDto } from "./dto/create-database.dto";
 import { DatabaseDto } from "./dto/database.dto";
 import { updateDatabaseDto } from "./dto/update-database.dto";
+import { Response } from 'express';
 
 const temporaryObject: DatabaseDto = {
     id: 'eb7a7202-028d-4b6a-b293-f8600a0a2592',
@@ -15,27 +16,37 @@ const temporaryObject: DatabaseDto = {
 export class DatabasesController {
 
     @Get(':id')
-    public getOne(@Param() id: string): DatabaseDto {
+    @HttpCode(HttpStatus.OK)
+    public getOne(@Res({ passthrough: true }) res: Response, @Param() id: string): DatabaseDto {
+        // @TODO Return internal server error when not found.
+        // res.status(HttpStatus.INTERNAL_SERVER_ERROR);
         return temporaryObject;
     }
 
     @Get()
-    public getAll(): DatabaseDto[] {
+    @HttpCode(HttpStatus.OK)
+    public getAll(@Res({ passthrough: true }) res: Response,): DatabaseDto[] {
         return [temporaryObject, temporaryObject, temporaryObject];
     }
 
     @Post()
-    public create(@Body() database: createDatabaseDto): DatabaseDto {
+    @HttpCode(HttpStatus.CREATED)
+    public create(@Res({ passthrough: true }) res: Response, @Body() database: createDatabaseDto): DatabaseDto {
+        // @TODO Return internal server error when object cant be created
         return temporaryObject;
     }
 
     @Patch()
-    public update(@Body() database: updateDatabaseDto): DatabaseDto {
+    @HttpCode(HttpStatus.OK)
+    public update(@Res({ passthrough: true }) res: Response, @Body() database: updateDatabaseDto): DatabaseDto {
+        // @TODO Return internal server error when object cant be updated
         return temporaryObject;
     }
 
     @Delete(':id')
-    public delete(@Param() id: string): void {
+    @HttpCode(HttpStatus.NO_CONTENT)
+    public delete(@Res({ passthrough: true }) res: Response, @Param() id: string): void {
+        // @TODO Return internal server error when object cant be deleted
         return;
     }
 
