1328acc89c26322975769a343f0a0f0d5709a12a
NielsPrins <NielsPrins@users.noreply.github.com>
2022-11-24 15:45:38 +0100

    Databases endpoint haalt data op uit database via TypeORM

--- backend/src/user/users.module.ts
+++ backend/src/user/users.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import {AppService} from "../app.service";
-import {UserController} from "./user.controller";
+import { UserController } from "./user.controller";
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
     controllers: [UserController],
-    providers: [UsersService, AppService],
+    providers: [UsersService],
 })
 export class UsersModule {
-
 }
