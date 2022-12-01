1ed856a64a294bfc92ccc0f235d1e7bdb211e206
Ruben WIjnbergen <S1127833@student.hsleiden.nl>
2022-12-01 14:05:33 +0100

    fixed .sh files to run and fixed SQL script to correctly reference the enum and the foreign keys

--- postgresql/init-z-schema-tables.sh
+++ postgresql/init-z-schema-tables.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$PG_ADMIN_USERNAME" --dbname "$PG_ADMIN_DATABASE" <<-EOSQL
@@ -10,7 +9,7 @@ CREATE TYPE $PG_DB_SCHEMA.type_enum AS ENUM ('redis', 'postgres');
 CREATE TABLE $PG_DB_SCHEMA.Database (
     "id" uuid NOT NULL,
     "name" character varying(50) NOT NULL,
-    "type" type_enum NOT NULL,
+    "type" $PG_DB_SCHEMA.type_enum NOT NULL,
     "creation_date_time" TIMESTAMP NOT NULL,
     CONSTRAINT "Database_pk" PRIMARY KEY ("id")
 );
@@ -27,14 +26,14 @@ CREATE TABLE $PG_DB_SCHEMA.Query (
     "storage_after_query" int NOT NULL,
     "database_id" uuid NOT NULL,
     CONSTRAINT "Query_pk" PRIMARY KEY ("creation_date_time","database_id"),
-    CONSTRAINT "Query_fk0" FOREIGN KEY ("database_id") REFERENCES "Database"("id")
+    CONSTRAINT "Query_fk0" FOREIGN KEY ("database_id") REFERENCES $PG_DB_SCHEMA.Database("id")
 );
 
 CREATE TABLE $PG_DB_SCHEMA.User_Owns_Database (
     "user_id" uuid NOT NULL,
     "database_id" uuid NOT NULL,
     CONSTRAINT "User_Owns_Database_pk" PRIMARY KEY ("user_id","database_id"),
-    CONSTRAINT "User_Owns_Database_fk0" FOREIGN KEY ("database_id") REFERENCES "Database"("id"),
-    CONSTRAINT "User_Owns_Database_fk1" FOREIGN KEY ("user_id") REFERENCES "User"("id")
+    CONSTRAINT "User_Owns_Database_fk0" FOREIGN KEY ("database_id") REFERENCES $PG_DB_SCHEMA.Database("id"),
+    CONSTRAINT "User_Owns_Database_fk1" FOREIGN KEY ("user_id") REFERENCES $PG_DB_SCHEMA.User("id")
 );
 EOSQL
