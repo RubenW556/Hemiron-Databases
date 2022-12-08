dadf147fc4713873e02d756144bc6f30cfbf3cf8
NielsPrins <NielsPrins@users.noreply.github.com>
2022-12-08 14:02:03 +0100

    Database tabel namen mogen niet uppercase zijn, dit snapt type orm niet

--- postgresql/init-z-schema-tables.sh
+++ postgresql/init-z-schema-tables.sh
@@ -6,7 +6,7 @@ CREATE SCHEMA $PG_DB_SCHEMA;
 
 CREATE TYPE $PG_DB_SCHEMA.type_enum AS ENUM ('redis', 'postgres');
 
-CREATE TABLE $PG_DB_SCHEMA.Database (
+CREATE TABLE $PG_DB_SCHEMA.database (
     "id" uuid NOT NULL,
     "name" character varying(50) NOT NULL,
     "type" $PG_DB_SCHEMA.type_enum NOT NULL,
@@ -14,26 +14,26 @@ CREATE TABLE $PG_DB_SCHEMA.Database (
     CONSTRAINT "Database_pk" PRIMARY KEY ("id")
 );
 
-CREATE TABLE $PG_DB_SCHEMA.User (
+CREATE TABLE $PG_DB_SCHEMA.user (
     "username" character varying(50) NOT NULL,
     "id" uuid NOT NULL,
     CONSTRAINT "User_pk" PRIMARY KEY ("id")
 );
 
-CREATE TABLE $PG_DB_SCHEMA.Query (
+CREATE TABLE $PG_DB_SCHEMA.query (
     "resource_used" int NOT NULL,
     "creation_date_time" TIMESTAMP NOT NULL,
     "storage_after_query" int NOT NULL,
     "database_id" uuid NOT NULL,
     CONSTRAINT "Query_pk" PRIMARY KEY ("creation_date_time","database_id"),
-    CONSTRAINT "Query_fk0" FOREIGN KEY ("database_id") REFERENCES $PG_DB_SCHEMA.Database("id")
+    CONSTRAINT "Query_fk0" FOREIGN KEY ("database_id") REFERENCES $PG_DB_SCHEMA.database("id")
 );
 
-CREATE TABLE $PG_DB_SCHEMA.User_Owns_Database (
+CREATE TABLE $PG_DB_SCHEMA.user_owns_database (
     "user_id" uuid NOT NULL,
     "database_id" uuid NOT NULL,
     CONSTRAINT "User_Owns_Database_pk" PRIMARY KEY ("user_id","database_id"),
-    CONSTRAINT "User_Owns_Database_fk0" FOREIGN KEY ("database_id") REFERENCES $PG_DB_SCHEMA.Database("id"),
-    CONSTRAINT "User_Owns_Database_fk1" FOREIGN KEY ("user_id") REFERENCES $PG_DB_SCHEMA.User("id")
+    CONSTRAINT "User_Owns_Database_fk0" FOREIGN KEY ("database_id") REFERENCES $PG_DB_SCHEMA.database("id"),
+    CONSTRAINT "User_Owns_Database_fk1" FOREIGN KEY ("user_id") REFERENCES $PG_DB_SCHEMA.user("id")
 );
 EOSQL
