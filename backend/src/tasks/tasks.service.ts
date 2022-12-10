6a83c85b565961315e44e424eb524be516489999
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2022-12-10 16:56:43 +0100

    task service file with example task that runs a log every 30 seconds

---
+++ backend/src/tasks/tasks.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+
+@Injectable()
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  handleCron() {
+    console.log(123);
+    this.logger.debug('Called when the current second is 45');
+  }
+}
