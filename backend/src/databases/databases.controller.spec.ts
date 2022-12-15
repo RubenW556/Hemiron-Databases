91c0d6064fdc2962051e6d9ca24438ba3d3323d4
Ruben WIjnbergen <S1127833@student.hsleiden.nl>
2022-12-15 11:35:17 +0100

    no message

--- backend/src/databases/databases.controller.spec.ts
+++ backend/src/databases/databases.controller.spec.ts
@@ -2,22 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DatabasesController } from './databases.controller';
 import {getRepositoryToken} from "@nestjs/typeorm";
 import {Database} from "./database.entity";
-import {UsersService} from "../user/users.service";
-import {User} from "../user/user.entity";
 import {DatabasesService} from "./databases.service";
+import {UserOwnsDatabaseService} from "../user-owns-database/user-owns-database.service";
+import {UserOwnsDatabase} from "../user-owns-database/user-owns-database.entity";
 
 describe('DatabasesController', () => {
   let controller: DatabasesController;
 
   const mockDatabaseRepository= {};
+  const mockUserOwnsDatabase= {};
+
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       providers: [
+        UserOwnsDatabaseService,
         DatabasesController,
         DatabasesService,
         {provide: getRepositoryToken(Database),
           useValue: mockDatabaseRepository,
+        },
+        {provide: getRepositoryToken(UserOwnsDatabase),
+          useValue: mockUserOwnsDatabase,
         }
       ]
     }).compile();
