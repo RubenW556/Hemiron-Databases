fea652982baac33d864e05912f4a0bad849ad403
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2022-12-18 00:57:36 +0100

    Create simple script to get database size

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,12 +1,24 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { MetricsService } from '../metrics/metrics.service';
 
 @Injectable()
 export class TasksService {
+  constructor(private metricsService: MetricsService) {
+    this.fetchAllDatabaseSizes().then((sizes) => {
+      this.logger.debug(sizes);
+    });
+  }
+
+  fetchAllDatabaseSizes() {
+    return this.metricsService.getAllDatabaseSizes();
+  }
+
   private readonly logger = new Logger(TasksService.name);
 
   @Cron(CronExpression.EVERY_30_SECONDS)
   handleCron() {
     this.logger.debug('Called every 30 seconds');
+
   }
 }
