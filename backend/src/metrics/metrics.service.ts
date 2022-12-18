a6a166f9a86138b97a2252c76d0b4082cc4c8fdc
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2022-12-18 17:36:09 +0100

    get size of databases from specifiic user only

--- backend/src/metrics/metrics.service.ts
+++ backend/src/metrics/metrics.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
-import {User} from "../user/user.entity";
 
 @Injectable()
 export class MetricsService {
@@ -9,7 +8,7 @@ export class MetricsService {
 
   /**
    * Gets size of single database
-   * @param databaseName
+   * @param {string} databaseName Name of Database to get size of
    */
   async getDatabaseSize(databaseName: string) {
     const postWithquery = await this.dataSource.query(
@@ -41,6 +40,31 @@ export class MetricsService {
         OFFSET $2;
       `,
       [3, 0],
+    );
+
+    return postWithquery;
+  }
+
+  /**
+   * Gets sizes of all databases of user
+   * @param {string} id UUID of requested user as string
+   * @param pageNumber
+   * @param pageSize
+   */
+  async getAllDatabaseSizesOfUser(id: string, pageNumber = 1, pageSize = 10) {
+    const limit = pageSize;
+    const offset = (pageNumber - 1) * pageSize;
+    const postWithquery = await this.dataSource.query(
+      `
+        SELECT t1.datname AS db_name,
+        pg_size_pretty(pg_database_size(t1.datname)) AS db_size
+        FROM pg_database AS t1
+        WHERE t1.datname LIKE $1
+        ORDER BY pg_database_size(t1.datname)
+        LIMIT $2
+        OFFSET $3;
+      `,
+      [`${id}.%`, limit, offset],
     );
 
     return postWithquery;
