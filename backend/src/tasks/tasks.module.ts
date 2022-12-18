4d863e57442ad9669f9765d1e74d7b0efb9b657b
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2022-12-18 17:36:23 +0100

    http imports

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MetricsService } from '../metrics/metrics.service';
+import { BillingService } from '../metrics/billing.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
-  providers: [TasksService, MetricsService],
+  imports: [
+    ScheduleModule.forRoot(),
+    HttpModule.register({
+      timeout: 5000,
+    }),
+  ],
+  providers: [TasksService, MetricsService, BillingService],
 })
 export class TasksModule {}
