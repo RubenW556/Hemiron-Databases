40a2e0c7dc81adb5bf189b123db13af357062b7e
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2022-12-23 13:35:18 +0100

    http import

--- backend/src/billing-integration/billing-integration.module.ts
+++ backend/src/billing-integration/billing-integration.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BillingService } from './billing.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+    }),
+  ],
   providers: [BillingService],
   exports: [BillingService],
 })
