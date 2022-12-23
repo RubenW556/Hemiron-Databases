d99eceb46b5a32fb627815a15f100a28739f45a5
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2022-12-23 14:41:29 +0100

    metrics controller endpoints

--- backend/src/metrics/metrics.controller.ts
+++ backend/src/metrics/metrics.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { MetricsService } from './metrics.service';
 
 @Controller('metrics')
@@ -8,5 +8,15 @@ export class MetricsController {
   @Get('test')
   getHello(): string {
     return this.metricsService.getHello();
+  }
+
+  @Get('databases/postgres/users')
+  getAllDatabaseMetrics(): any {
+    return this.metricsService.getAllDatabaseSizesOfAllUsers();
+  }
+
+  @Get('databases/postgres/users/:id')
+  getAllDatabaseMetricsOfUser(@Param('id') id: string): any {
+    return this.metricsService.getAllPostgresDatabaseSizesOfSingleUser(id);
   }
 }
