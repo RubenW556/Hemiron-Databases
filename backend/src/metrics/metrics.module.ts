4161ec0a037d7fc2827c8699640b05e841f87f2e
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2022-12-23 14:41:42 +0100

    metrics module to export its service

--- backend/src/metrics/metrics.module.ts
+++ backend/src/metrics/metrics.module.ts
@@ -3,8 +3,8 @@ import { MetricsService } from './metrics.service';
 import { MetricsController } from './metrics.controller';
 
 @Module({
-  imports: [],
   controllers: [MetricsController],
   providers: [MetricsService],
+  exports: [MetricsService],
 })
 export class MetricsModule {}
