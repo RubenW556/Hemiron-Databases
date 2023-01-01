c1fbcf187c5e2393c0f14fad127251ff31d59c3e
suley <s1115672@student.hsleiden.nl>
2023-01-01 15:18:02 +0100

    Added a solid start for redis.controller.ts and redis.service.ts.
    Some small tests all over to check if redis is working

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Inject} from '@nestjs/common';
+import {Controller, Get, Inject, Res} from '@nestjs/common';
 import { AppService } from './app.service';
 import { Redis } from 'ioredis';
 import { IORedisKey } from './redis/redis.module';
@@ -10,13 +10,21 @@ export class AppController {
       @Inject(IORedisKey) private readonly redis: Redis  ) {
   }
 
-  //@Get('test')
-  //getHello(): string {
-  //  return this.appService.getHello();
-  //}
-
   @Get('test')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('test1')
   async get(key: string): Promise<string> {
     return this.redis.get(key);
   }
+
+  @Get('test2')
+  async setAndGetValue(key: string): Promise<string> {
+    const value = 'hell-o-worl';
+    await this.redis.set(key, value);
+    return this.redis.get(key);
+  }
+
 }
---
+++ backend/src/redis/redis.controller.ts
@@ -0,0 +1,94 @@
+import {Body, Controller, Get, HttpCode, HttpStatus, Inject, Param, Post, Res} from '@nestjs/common';
+import { Redis } from 'ioredis';
+import { IORedisKey } from './redis.module';
+import { Response } from "express";
+import { hash } from 'typeorm/util/stringutils';
+import {DatabasesService} from "../databases/databases.service";
+import {UserOwnsDatabaseService} from "../user-owns-database/user-owns-database.service";
+import {UsersService} from "../user/users.service";
+import {RedisService} from "./redis.service";
+import {CreateDatabaseDto} from "../databases/dto/create-database.dto";
+import {Database} from "../databases/database.entity";
+
+@Controller('redis')
+@Controller()
+export class RedisController {
+    constructor(
+        private redisService: RedisService,
+    ) {}
+
+    @Get('test1/:key')
+    async setAndGetValue(@Param('key') key: string): Promise<string> {
+        const value = 'hell-o-worl';
+        return this.redisService.test1(key, value);
+    }
+
+    @Get('test2')
+    async getInfo(): Promise<string> {
+        return this.redisService.test2();
+    }
+
+    @Get('test3')
+    async getInfo2(): Promise<string> {
+        return this.redisService.test3();
+    }
+
+    @Get('test4')
+    async getInfo3(): Promise<string> {
+        return this.redisService.test4();
+    }
+
+    @Get(':username')
+    @HttpCode(HttpStatus.OK)
+    public async delete(
+        @Res({ passthrough: true }) res: Response,
+        @Param('username') username: string,
+    ): Promise<string> {
+    try {
+        // select the database corresponding to the user
+        await this.redisService.select(hash(username));
+
+        // get the value of the 'data' key from the selected database
+        const data = await this.redisService.get('data');
+        return data;
+    }
+    catch (e) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async create(): Promise<void> {
+        const dbs = await this.redis.config('GET', 'databases');
+        const newDb = parseInt(dbs.databases, 10) + 1;
+        await this.redis.config('SET', 'databases', newDb.toString());
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED) //todo validatioon
+    public async create(
+        @Res({ passthrough: true }) res: Response,
+        @Body() createDatabaseDto: CreateDatabaseDto,
+    ): Promise<Database> {
+        try {
+            const userMakingRequest = res.locals.userMakingRequest;
+
+            await this.usersService.findOne(userMakingRequest.id);
+
+            const insertResult = await this.databasesService.insert(
+                createDatabaseDto,
+            );
+            const newDatabaseId = insertResult.identifiers[0].id;
+
+            await this.userOwnsDatabaseService.insert(
+                newDatabaseId,
+                userMakingRequest.id,
+            );
+            return await this.databasesService.findOne(newDatabaseId);
+        } catch (e) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
