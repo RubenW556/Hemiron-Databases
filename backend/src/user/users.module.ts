52a0c1259e650ae67b3fe257f7600231c86bcd37
Ruben WIjnbergen <S1127833@student.hsleiden.nl>
2023-01-02 16:19:06 +0100

    metrics service in module

--- backend/src/user/users.module.ts
+++ backend/src/user/users.module.ts
@@ -3,13 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
-import { MetricsService } from '../metrics/metrics.service';
 import { DatabaseManagementService } from '../metaDatabaseManagement/databaseManagement.Service';
+import {MetricsModule} from "../metrics/metrics.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), MetricsModule],
   exports: [UsersService],
   controllers: [UsersController],
-  providers: [UsersService, MetricsService, DatabaseManagementService],
+  providers: [UsersService, DatabaseManagementService],
 })
 export class UsersModule {}
