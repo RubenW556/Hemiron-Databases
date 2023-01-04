92e683f015adb538f6bc41f9c926563667893cca
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2023-01-04 22:45:58 +0100

    metrics controller uitgewerkt met responses en basic validation

--- backend/src/metrics/metrics.controller.ts
+++ backend/src/metrics/metrics.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Logger,
+  Param,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { MetricsService } from './metrics.service';
+import { Request, Response } from 'express';
 
 @Controller('metrics')
 export class MetricsController {
+  private readonly logger = new Logger(MetricsController.name);
+
   constructor(private metricsService: MetricsService) {}
 
   @Get('test')
-  getHello(): string {
-    return this.metricsService.getHello();
+  getHello(@Req() request: Request, @Res() response: Response) {
+    response.status(HttpStatus.OK).send({ message: `test` });
   }
 
   @Get('databases/postgres/users')
@@ -16,7 +27,37 @@ export class MetricsController {
   }
 
   @Get('databases/postgres/users/:id')
-  getAllDatabaseMetricsOfUser(@Param('id') id: string): any {
-    return this.metricsService.getAllPostgresDatabaseSizesOfSingleUser(id);
+  async getAllDatabaseMetricsOfUser(
+    @Req() request: Request,
+    @Res() response: Response,
+    @Param('id') id: string,
+  ): Promise<any> {
+    if (!id) {
+      const responseMessage = `No valid id provided.`;
+      this.logger.debug(responseMessage);
+      return response
+        .status(HttpStatus.BAD_REQUEST)
+        .send({ message: responseMessage });
+    }
+    const size =
+      await this.metricsService.getAllPostgresDatabaseSizesOfSingleUser(id);
+    response.status(HttpStatus.OK).send({ sizeUsage: size });
+  }
+
+  @Get('databases/postgres/databases/:id')
+  async getSizeOfSingleDatabase(
+    @Req() request: Request,
+    @Res() response: Response,
+    @Param('id') id: number,
+  ): Promise<any> {
+    if (!id) {
+      const responseMessage = `No valid id provided.`;
+      this.logger.debug(responseMessage);
+      return response
+        .status(HttpStatus.BAD_REQUEST)
+        .send({ message: responseMessage });
+    }
+    const size = await this.metricsService.getDatabaseSize(id);
+    response.status(HttpStatus.OK).send({ size: size });
   }
 }
