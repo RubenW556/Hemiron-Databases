a03ae5a71f9887e595d091f5214dc5a95502d48b
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2023-01-06 16:46:49 +0100

    Cleanup code for tasks service

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { MetricsService } from '../metrics/metrics.service';
 import { BillingIntegrationService } from '../billing-integration/billing-integration.service';
 import { PatchUserDatabaseMetricsDto } from '../billing-integration/patchUserDatabaseMetrics.dto';
+import { UsersService } from '../user/users.service';
 
 @Injectable()
 export class TasksService {
@@ -11,41 +12,54 @@ export class TasksService {
   constructor(
     private metricsService: MetricsService,
     private billingService: BillingIntegrationService,
-  ) {
-    //test //todo move into cron
-    this.initiateUserPostgresMetrics();
-  }
+    private usersService: UsersService,
+  ) {}
 
   async initiateUserPostgresMetrics() {
-    // const users = await this.usersService.findAll();
-    const users = [{ id: 'b45fc742-be1c-4c22-957c-3dd352743074' }]; //todo change back to line above
-    for (const user of users) {
-      const uuid = user.id;
-      let size;
-      try {
-        size = await this.metricsService.getCombinedPostgresMetricsOfUser(uuid);
-      } catch (e) {
-        continue;
-      }
-      this.logger.debug(size);
-      const payload: PatchUserDatabaseMetricsDto = {
-        size: size,
-        userId: uuid,
-      };
+    try {
+      const users = await this.usersService.findAll();
+      for (const user of users) {
+        const uuid = user.id;
+        let size;
+        try {
+          size = await this.metricsService.getCombinedPostgresMetricsOfUser(
+            uuid,
+          );
+        } catch (e) {
+          this.logger.debug(
+            `Failed to get Postgres metrics for uuid ${uuid}... Skipping....`,
+          );
 
-      this.billingService
-        .patchPostgresUserDataToBilling(payload)
-        .then((response) => {
-          this.logger.debug(response);
-        })
-        .catch((error) => {
-          this.logger.error(error);
-        });
+          continue;
+        }
+        this.logger.debug(size);
+        const payload: PatchUserDatabaseMetricsDto = {
+          size: size,
+          userId: uuid,
+        };
+
+        this.billingService
+          .patchPostgresUserDataToBilling(payload)
+          .then((response) => {
+            this.logger.log(
+              `Successfully patched postgres usage for user ${uuid}`,
+            );
+            this.logger.debug(response);
+          })
+          .catch((error) => {
+            this.logger.error(error);
+          });
+      }
+    } catch (e) {
+      this.logger.error(e);
     }
   }
 
   @Cron(CronExpression.EVERY_3_HOURS)
   handleCron() {
     this.logger.log('Initiating periodic metrics integration.');
+    this.initiateUserPostgresMetrics().then(() => {
+      this.logger.log('Concluding periodic metrics integration.');
+    });
   }
 }
