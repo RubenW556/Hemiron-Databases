b63d02d4234bb43389e62564aa0bc475e8a9ef40
Ottenhof 1109588 <s1109588@student.hsleiden.nl>
2023-01-09 18:58:45 +0100

    remove comment

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -19,10 +19,4 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
-  // //for assertion failure
-  // describe('root', () => {
-  //   it('should return "Hello World!"', () => {
-  //     expect(appController.getHello()).toBe('Hello Worldd!');
-  //   });
-  // });
 });
